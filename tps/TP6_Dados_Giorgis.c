0d111226f3b47c9bcda68928a8f8c1f6445c9d9c
Carpincho99 <ezegiorgis@hotmail.com>
2021-08-26 11:17:21 -0300

    add funtions

--- tps/TP6_Dados_Giorgis.c
+++ tps/TP6_Dados_Giorgis.c
@@ -2,26 +2,44 @@
 #include<stdlib.h>
 #include<time.h>
 
+#define M 36000 //Cantidad de lanzamientos
+
+int lanzarDados ();
+void cargarArr (int*, int);
+void printArr (int*);
+
 int main(void){
   int arr[11] = {0};
   int sumDados;
 
   srand(time(NULL)); //set rand seed
 
-  for (int i = 0; i < 36000; i++) {
-    sumDados = (1+rand()%6) + (1+rand()%6);
-    for (int j = 0; j < 11; j++) {
-      if (sumDados == j+2) {
-        arr[j]++; 
-      } 
-    }
+  for (int i = 0; i < M; i++) {
+    sumDados = lanzarDados() + lanzarDados();
+    cargarArr(arr, sumDados);    
   }
 
-  printf("Valor   Cantidad de veces\n\n");
-  for (int i = 0; i < 11; i++) {
-    printf("%2d:%12d\n", i+2, arr[i]);
-  }
+  printArr(arr);
 
   return 0;
 }
 
+
+int lanzarDados(){
+  return (1+rand()%6);
+}
+
+void cargarArr(int arr[], int sumDados) {
+  for (int j = 0; j < 11; j++) {
+    if (sumDados == j+2) {
+      arr[j]++; 
+    } 
+  }
+}
+
+void printArr(int arr[]) {
+  printf("Valor   Cantidad de veces\n\n");
+  for (int i = 0; i < 11; i++) {
+    printf("%2d:%12d\n", i+2, arr[i]);
+  }
+}
